4f2b669079b090fcf694a3abe0e0e8177fb47a56
Simon Kumets <sjkumets@gmail.com>
2015-10-20 13:20:16 -0500

    added javadoc to Person.java

--- src/studybuddy/Person.java
+++ src/studybuddy/Person.java
@@ -1,19 +1,43 @@
 package studybuddy;
 
-public abstract class Person {
+/** 
+* Person class defines a user  
+* Includes basic information of any type of user
+* This class can be extended to define more specific users
+*/
+public class Person {
 	protected String firstName;
 	protected String lastName;
 	protected String email;
 	
+	/** 
+	 * @return email of person
+	 */
 	public String getEmail(){ return email; }
 	
+	/** 
+	 * @return first name of person
+	 */
 	public String getFirstName(){ return firstName; }
-
+	
+	
+	/** 
+	 * @return last name of person
+	 */
 	public String getLastName(){ return lastName; }
 	
-	public void setEmail(String temp){ email = temp; }
+	/** 
+	 * @param email address of person
+	 */
+	public void setEmail(String email){ this.email = email; }
 	
+	/** 
+	 * @param first name of person
+	 */
 	public void setFirstName(String name){ firstName = name; }
 	
+	/** 
+	 * @param last name of person
+	 */
 	public void setLastName(String name){ lastName = name;}
 }
