60a12efc0551cd06776aebea5a167cfa7cc14be2
Simon Kumets <sjkumets@gmail.com>
2015-10-20 11:00:56 -0500

    add String parameter to notifyObservers

--- src/studybuddy/Subject.java
+++ src/studybuddy/Subject.java
@@ -3,5 +3,5 @@ package studybuddy;
 public interface Subject {
     public void registerObserver(Observer observer);
     public void removeObserver(Observer observer);
-    public void notifyObservers();
+    public void notifyObservers(String message);
 }
