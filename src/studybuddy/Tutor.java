f6d6f1f7060ea086f92dfcb6dbf674ec1b893efe
Christopher <challgil@gmail.com>
2015-10-20 10:26:07 -0500

    Adding tutor objects

--- src/studybuddy/Tutor.java
+++ src/studybuddy/Tutor.java
@@ -1,5 +1,27 @@
 package studybuddy;
 
-public class Tutor extends Person {
+import java.util.ArrayList;
+
+public class Tutor extends Person implements Subject{
+	
+	private double price;
+	private ArrayList<String> subjects = new ArrayList<String>();
+	private ArrayList<Student> subscribers = new ArrayList<Student>();
+	
+	public void setPrice(double number){ price = number;}
+	
+	public void addSubject(String subject){ subjects.add(subject);}
+	
+	public double getPrice(){ return price;}
+	
+	public ArrayList<String> egtSubjects(){ return subjects;}
+	
+	public void subscribe(Student name){ subscribers.add(name);}
+	
+	public void unsubscribe(Student name){ 
+		int index = subscribers.indexOf(name);
+		subscribers.remove(index);
+	}
+	
 	
 }
