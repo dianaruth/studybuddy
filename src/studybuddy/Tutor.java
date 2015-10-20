e81731ae5676d668fe7d3f4a14e69418798c12a0
Christopher <challgil@gmail.com>
2015-10-20 11:37:20 -0500

    yeah

---
+++ src/studybuddy/Tutor.java
@@ -0,0 +1,53 @@
+package studybuddy;
+
+import java.util.ArrayList;
+
+public class Tutor extends Person implements Subject{
+	
+	private double price;
+	private ArrayList<String> subjects = new ArrayList<String>();
+	private ArrayList<Observer> subscribers = new ArrayList<Observer>();
+	
+	public void setPrice(double number){ 
+		price = number;
+		String msg = new String(firstName + " " + lastName + " has updated their price to " + number + ".");
+		notifyObservers(msg);
+	}
+	
+	public void addSubject(String subject){
+		subjects.add(subject);
+		notifyObservers(firstName + " " + lastName + " has added the subject " + subject + ".");
+	}
+	
+	public void removeSubject(String subject)
+	{
+		int i = subjects.indexOf(subject);
+		subjects.remove(i);
+		notifyObservers(firstName + " " + lastName + " has removed the following subject from their list of tutored subjects: " + subject + ".");
+		
+	}
+	
+	public double getPrice(){ return price;}
+	
+	public ArrayList<String> getSubjects(){ return subjects;}
+	
+	public void registerObserver(Observer name){ subscribers.add(name);}
+	
+	public void removeObserver(Observer name){ 
+		int index = subscribers.indexOf(name);
+		subscribers.remove(index);
+	}
+
+	@Override
+	public void notifyObservers(String msg) {
+		for(int i = 0; i < subscribers.size(); i++)
+		{
+			subscribers.get(i).update(msg);
+		}
+		
+	}
+
+	
+	
+	
+}
