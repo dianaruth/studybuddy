55febc442831bbef168354218eb465b8a8500b0f
Simon Kumets <sjkumets@gmail.com>
2015-10-20 12:47:29 -0500

    add javadoc to observer and subject interfaces

--- src/studybuddy/Observer.java
+++ src/studybuddy/Observer.java
@@ -1,5 +1,12 @@
 package studybuddy;
 
+/**
+ * 
+ * Observer.java
+ * Interface class for observer 
+ *implemented by all observer
+ * 
+ */
 public interface Observer {
 	public void update(String updateMessage);
 	public void setSubject(Subject subject);
