687bb64fe79050dc1b342098f20892c1bbcd1863
Simon Kumets <sjkumets@gmail.com>
2015-10-20 10:46:44 -0500

    removed getUpdate()

--- src/studybuddy/Subject.java
+++ src/studybuddy/Subject.java
@@ -4,5 +4,4 @@ public interface Subject {
     public void registerObserver(Observer observer);
     public void removeObserver(Observer observer);
     public void notifyObservers();
-    public Object getUpdate();
 }
