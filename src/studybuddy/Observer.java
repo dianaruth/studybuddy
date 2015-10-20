cbdce7de298a5b2b23cb0ef177c23e7e537259f2
Simon Kumets <sjkumets@gmail.com>
2015-10-20 10:29:30 -0500

    adding Observer.java and subject.java

---
+++ src/studybuddy/Observer.java
@@ -0,0 +1,6 @@
+package studybuddy;
+
+public interface Observer {
+	public void update();
+	public void setSubject(Subject subject);
+}
---
+++ src/studybuddy/Subject.java
@@ -0,0 +1,8 @@
+package studybuddy;
+
+public interface Subject {
+    public void registerObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObservers();
+    public Object getUpdate();
+}
